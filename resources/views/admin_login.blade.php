3da2c1d0da0aebd0871dcb2e1eb0c8bb6d411182
Aiswarya K <aiswarya3777@gmail.com>
2022-06-02 12:02:18 +0530

    hrms project

---
+++ resources/views/admin_login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <style>
+        .center {
+            margin: auto;
+            width: 60%;
+            padding: 10px;
+            padding-top: 170px;
+                }
+                .content{
+                  background-color: cornflowerblue;  
+                }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 center"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 content">
+                <form action="">
+                    <h1 style="text-align:center;">LOGIN</h1>
+                    <div><label for="username">USER NAME</label>
+                        <input type="text">
+                    </div>
+                    <div> <label for="password">PASSWORD</label>
+                        <input type="text">
+                    </div>
+                    <center><button type="button" class="btn btn-dark">LOGIN</button></center>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
